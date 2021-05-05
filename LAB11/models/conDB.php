05a722787fcf4433f11b2a2b115031be5a804cd6
620112418023 <620112418023@bru.ac.th>
2021-05-05 12:32:52 +0700

    LAB11

---
+++ LAB11/models/conDB.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$host = "sql104.byethost3.com";
+$user = "b3_28403706";
+$pass = "0917282840";
+$db= "b3_28403706_MEMBER";
+
+try{
+    $conn = new mysqli($host,$user,$pass,$db);
+    // echo "Connected !!";
+}
+catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+?>
